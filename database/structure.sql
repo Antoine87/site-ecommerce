cd2ce4e2869f085d58d50f02934797d443fa9f3b
Stéphane Emploi <moi@moi.com>
2016-11-29 17:17:26 +0100

    ajout des tables panier et commandes

--- database/structure.sql
+++ database/structure.sql
@@ -149,7 +149,87 @@ CREATE TABLE `ecommerce`.`roles_auteurs` (
   PRIMARY KEY(id_role)
 )ENGINE = Innodb;
 
+-- ----------------------------------------------------
+-- Création de la table collection
+-- ----------------------------------------------------
+CREATE TABLE collection (
+  id_collection MEDIUMINT UNSIGNED AUTO_INCREMENT,
+  collection VARCHAR(50) NOT NULL,
+  id_editeur MEDIUMINT UNSIGNED NOT NULL,
+  PRIMARY KEY (id_collection),
+  CONSTRAINT collection_to_editeur
+  FOREIGN KEY (id_editeur)
+  REFERENCES editeurs(id_editeur)
+)ENGINE = Innodb;
 
-INSERT INTO ecommerce.mode_de_paiement (mode_de_paiement) VALUES ('espece');
-INSERT INTO ecommerce.mode_de_paiement (mode_de_paiement) VALUES ('carte bleu');
-INSERT INTO ecommerce.mode_de_paiement (mode_de_paiement) VALUES ('cheque');
+-- -----------------------------------------------------
+-- Table ecommerce.commandes
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS ecommerce.commandes (
+  num_commande INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  date_commande DATE NOT NULL,
+  date_expedition DATE ,
+  date_livraison DATE ,
+  id_client INT UNSIGNED NOT NULL,
+  id_mode_livraison TINYINT UNSIGNED NOT NULL,
+  id_coupon MEDIUMINT UNSIGNED NOT NULL,
+  id_adresse INT UNSIGNED NOT NULL,
+  id_statut TINYINT UNSIGNED NOT NULL,
+  PRIMARY KEY (num_commande),
+  INDEX fk_commandes_clients_idx (id_client ASC),
+  UNIQUE INDEX id_client_UNIQUE (id_client ASC),
+  INDEX fk_commandes_mode_livraison1_idx (id_mode_livraison ASC),
+  INDEX fk_commandes_coupons1_idx (id_coupon ASC),
+  INDEX fk_commandes_adresses1_idx (id_adresse ASC),
+  UNIQUE INDEX id_mode_livraison_UNIQUE (id_mode_livraison ASC),
+  UNIQUE INDEX coupons_idCoupon_UNIQUE (id_coupon ASC),
+  UNIQUE INDEX adresses_id_adresses_UNIQUE (id_adresse ASC),
+  INDEX fk_commandes_statut_de_commande1_idx (id_statut ASC),
+  UNIQUE INDEX id_statut_UNIQUE (id_statut ASC),
+  CONSTRAINT fk_commandes_clients
+  FOREIGN KEY (id_client)
+  REFERENCES ecommerce.clients (id_client)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_commandes_mode_livraison1
+  FOREIGN KEY (id_mode_livraison)
+  REFERENCES ecommerce.modes_livraison (id_mode_livraison)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_commandes_coupons1
+  FOREIGN KEY (id_coupon)
+  REFERENCES ecommerce.Coupons (id_coupon)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_commandes_adresses1
+  FOREIGN KEY (id_adresse)
+  REFERENCES ecommerce.adresses (id_adresse)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_commandes_statut_de_commande1
+  FOREIGN KEY (id_statut)
+  REFERENCES ecommerce.statuts_de_commande (id_statut)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
+
+-- ----------------------------------------------------
+-- Création de la table panier
+-- ----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`Panier` (
+  `Quantite` VARCHAR(30) NULL,
+  `Produit` INT NOT NULL,
+  `Client` INT NOT NULL,
+  INDEX `fk_Panier_livres_idx` (`Produit` ASC),
+  INDEX `fk_Panier_clients1_idx` (`Client` ASC),
+  CONSTRAINT `fk_Panier_livres`
+  FOREIGN KEY (`Produit`)
+  REFERENCES ecommerce.`livres` (`id_livre`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Panier_clients1`
+  FOREIGN KEY (`Client`)
+  REFERENCES ecommerce.`clients` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
