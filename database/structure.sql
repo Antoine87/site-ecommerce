094105c1d1ce10a42b89383181de7a2c69a7937f
Didier Cuper <truc@free.fr>
2016-11-29 13:16:00 +0100

    ajout table auteur (Didier)

--- database/structure.sql
+++ database/structure.sql
@@ -5,3 +5,14 @@
 DROP DATABASE IF EXISTS ecommerce;
 
 CREATE DATABASE ecommerce;
+
+-- -----------------------------------------------------
+-- Table ecommerce.Auteurs
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS ecommerce.Auteurs (
+  id_auteur INT NOT NULL AUTO_INCREMENT,
+  nom_auteur VARCHAR(45) NOT NULL,
+  prenom_auteur VARCHAR(45) NOT NULL,
+  biographie VARCHAR(250) NOT NULL,
+  PRIMARY KEY (id_auteur))
+ENGINE = InnoDB;
