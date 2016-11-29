5b7914913df354397e9744522402413283099720
Stéphane Emploi <moi@moi.com>
2016-11-29 16:08:52 +0100

    Corrections et Mise au propre du DDL

--- database/structure.sql
+++ database/structure.sql
@@ -2,6 +2,8 @@
 * Structure de données de l'application ecommerce
  ****************************************************/
 
+DROP DATABASE IF EXISTS ecommerce;
+
 -- Création de la base de données
 CREATE DATABASE ecommerce
 CHARACTER SET = utf8 COLLATE = utf8_general_ci;
@@ -12,8 +14,8 @@ USE ecommerce;
 -- Table langues
 -- ----------------------------------------------
 CREATE TABLE langues (
-id_langue SMALLINT UNSIGNED AUTO_INCREMENT,
-nom_langue VARCHAR(20) NOT NULL,
+id_langue TINYINT UNSIGNED AUTO_INCREMENT,
+nom_langue VARCHAR(30) NOT NULL,
   PRIMARY KEY (id_langue)
 ) ENGINE = INNODB;
 
@@ -21,22 +23,23 @@ nom_langue VARCHAR(20) NOT NULL,
 -- Table `ecommerce`.`auteurs`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS ecommerce.auteurs (
-  id_auteur INT NOT NULL AUTO_INCREMENT,
+  id_auteur MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
   nom_auteur VARCHAR(45) NOT NULL,
   prenom_auteur VARCHAR(45) NOT NULL,
-  biographie VARCHAR(250) NOT NULL,
-  PRIMARY KEY (id_auteur));
+  biographie TEXT NOT NULL,
+  PRIMARY KEY (id_auteur)
+)ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `ecommerce`.`clients`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`clients` (
-  `id_client` INT NOT NULL,
-  `nom` VARCHAR(45) NULL,
-  `prenom` VARCHAR(45) NULL,
-  `email` VARCHAR(100) NULL,
-  `mot_de_passe` VARCHAR(45) NULL,
-  `date_naissance` DATE NULL,
+  `id_client` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `prenom` VARCHAR(45),
+  `email` VARCHAR(50) NOT NULL,
+  `mot_de_passe` VARCHAR(128) NOT NULL,
+  `date_naissance` DATE,
   PRIMARY KEY (`id_client`))
 ENGINE = InnoDB;
 
@@ -45,13 +48,13 @@ ENGINE = InnoDB;
 -- Table `ecommerce`.`telephones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`telephones` (
-  `id_telephones` INT NOT NULL,
-  `numero_telephone` VARCHAR(15) NULL,
-  `clients_id_client` INT NOT NULL,
+  `id_telephones` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `numero_telephone` VARCHAR(10) NULL,
+  `id_client` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id_telephones`),
-  INDEX `fk_telephones_clients1_idx` (`clients_id_client` ASC),
+  INDEX `fk_telephones_clients1_idx` (`id_client` ASC),
   CONSTRAINT `fk_telephones_clients1`
-    FOREIGN KEY (`clients_id_client`)
+    FOREIGN KEY (`id_client`)
     REFERENCES `ecommerce`.`clients` (`id_client`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -62,16 +65,16 @@ ENGINE = InnoDB;
 -- Table `ecommerce`.`adresses`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`adresses` (
-  `id_adresse` INT NOT NULL,
+  `id_adresse` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `adresse` VARCHAR(45) NULL,
   `code_postal` VARCHAR(5) NULL,
   `ville` VARCHAR(45) NULL,
-  `adresse_facturation` INT NULL,
-  `clients_id_client` INT NOT NULL,
+  `est_adresse_facturation` BIT NOT NULL DEFAULT 1,
+  `id_client` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id_adresse`),
-  INDEX `fk_adresses_clients1_idx` (`clients_id_client` ASC),
+  INDEX `fk_adresses_clients1_idx` (`id_client` ASC),
   CONSTRAINT `fk_adresses_clients1`
-    FOREIGN KEY (`clients_id_client`)
+    FOREIGN KEY (`id_client`)
     REFERENCES `ecommerce`.`clients` (`id_client`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -79,7 +82,7 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`Coupon`
+-- Table `ecommerce`.`statut_de_commande`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS ecommerce.statut_de_commmande (
     statut VARCHAR(255),
@@ -92,7 +95,7 @@ CREATE TABLE IF NOT EXISTS ecommerce.statut_de_commmande (
 -- Table `ecommerce`.`Coupon`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`Coupon` (
-  `idCoupon` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idCoupon` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Date_debut` DATE NULL,
   `Date_fin` DATE NULL,
   `Remise` FLOAT NULL,
@@ -100,53 +103,47 @@ CREATE TABLE IF NOT EXISTS `ecommerce`.`Coupon` (
 ENGINE = InnoDB;
 
 
---------------------------------------------------------
+-- ------------------------------------------------------
 --  TABLE ecommerce.mode_de_paiement
---------------------------------------------------------
+-- ------------------------------------------------------
 CREATE TABLE IF NOT EXISTS ecommerce.mode_de_paiement (
-  id_mode_de_paiement INT NOT NULL,
+  id_mode_de_paiement TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
   mode_de_paiement VARCHAR(45) NOT NULL,
   PRIMARY KEY (id_mode_de_paiement),
-  UNIQUE INDEX id_mode_de_paiement_UNIQUE (id_mode_de_paiement ASC))
-  ENGINE = InnoDB;
+  UNIQUE INDEX id_mode_de_paiement_UNIQUE (id_mode_de_paiement ASC)
+) ENGINE = InnoDB;
 
 
---------------------------------------------------------
+-- ------------------------------------------------------
 --  TABLE ecommerce.mode_livraison
---------------------------------------------------------
+-- ------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ecommerce`.`mode_livraison` (
-  `id_mode_livraison` INT NOT NULL,
+  `id_mode_livraison` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `description_mode_livraison` VARCHAR(45) NULL,
-  `tarif_livraison` DECIMAL(2) NULL,
+  `tarif_livraison` DECIMAL(5,2) NOT NULL DEFAULT 0.0,
   `delais_livraison` INT NULL,
   PRIMARY KEY (`id_mode_livraison`))
 ENGINE = InnoDB;
 
 
---------------------------------------------------------
---  TABLE ecommerce.mode_editeurs
---------------------------------------------------------
-
--- Export de données de la table e_commerce.éditeurs : ~3 rows (environ)
-DELETE FROM `éditeurs`;
-ALTER TABLE `éditeurs` DISABLE KEYS ;
-INSERT INTO `éditeurs` (`ID`, `Nom`) VALUES
-	(1, 'MICHEL LAFON'),
-	(2, 'John Tiffany'),
-	(3, 'Carole Neel ');
-ALTER TABLE `éditeurs` ENABLE KEYS ;
-SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') ;
-SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) ;
-SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT ;
+-- ------------------------------------------------------
+--  TABLE ecommerce.mode_editeur
+-- ------------------------------------------------------
+CREATE TABLE `ecommerce`.`editeurs` (
+  id_editeur MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nom_editeur VARCHAR(50) NOT NULL,
+  PRIMARY KEY(id_editeur)
+)ENGINE = Innodb;
 
 
---------------------------------------------------------
+-- ------------------------------------------------------
 --  TABLE ecommerce.roles_auteurs
---------------------------------------------------------
+-- ------------------------------------------------------
 CREATE TABLE `ecommerce`.`roles_auteurs` (
-  id_role INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  id_role TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  role VARCHAR(20) NOT NULL,
   PRIMARY KEY(id_role)
-);
+)ENGINE = Innodb;
 
 
 INSERT INTO ecommerce.mode_de_paiement (mode_de_paiement) VALUES ('espece');
