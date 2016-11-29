5db5d9392d1bc7548cdf1312bc6452a67db4550d
Cedric Nicolas <moi@moi.com>
2016-11-29 15:35:05 +0100

    Merge branch 'master' of https://github.com/m2i-formation-cdi/site-ecommerce
    # Conflicts:
    #	database/structure.sql

--- database/structure.sql
+++ database/structure.sql
@@ -1,17 +1,10 @@
 /****************************************************
 * Structure de données de l'application ecommerce
  ****************************************************/
-
-DROP DATABASE IF EXISTS ecommerce;
-
-<<<<<<< HEAD
 CREATE DATABASE ecommerce;
 
-CREATE TABLE [IF NOT EXISTS] statut_de_commmande (
+CREATE TABLE IF NOT EXISTS statut_de_commmande (
     statut VARCHAR(255),
-    [PRIMARY KEY (statut)]
-)
-[ENGINE=innoDB];
-=======
-CREATE DATABASE ecommerce;
->>>>>>> origin/master
+    PRIMARY KEY (statut))
+ENGINE = innoDB;
+
