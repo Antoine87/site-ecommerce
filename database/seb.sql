1b6c968f83232a4431e909bac65901e29eb492ca
sebbrec <titi@grominet.com>
2016-11-29 17:04:05 +0100

    seb

--- database/seb.sql
+++ database/seb.sql
@@ -13,6 +13,8 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
 
+///  les fk sont à redéfinir
+
 
 INDEX `fk_paiement_clients1` (`id_client` ASC),
   CONSTRAINT `fk_paiment_clients1`
@@ -21,4 +23,14 @@ INDEX `fk_paiement_clients1` (`id_client` ASC),
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+INDEX `fk_paiement_mode_paiement1` (`id_client` ASC),
+  CONSTRAINT `fk_paiement_mode_paiement`
+    FOREIGN KEY (`id_mode_paiement`)
+    REFERENCES `ecommerce`.`clients` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
