34679ec6b44ca96ae1b8106ece423ebe6562088c
PE <PE>
2016-11-29 16:45:41 +0100

    tables role auteur/langues/editeur

--- database/Pascal.sql
+++ database/Pascal.sql
@@ -1,6 +1,6 @@
 TRUNCATE `ecommerce`.`langues`;
 TRUNCATE `ecommerce`.`editeurs`;
 TRUNCATE `ecommerce`.`roles_auteurs`;
-INSERT INTO `langues` (`id_langue`, `nom_langue`) VALUES (NULL, 'fran�ais'), (NULL, 'anglais');
-INSERT INTO `editeurs` (`id_editeur`, `nom_editeur`) VALUES (NULL, 'POCKET'), (NULL, 'Albin Michel'), (NULL, 'Le Livre de Poche'), (NULL, 'Gallimard'), (NULL, 'Les Editions Pers�e');
+INSERT INTO `langues` (`id_langue`, `nom_langue`) VALUES (NULL, 'français'), (NULL, 'anglais');
+INSERT INTO `editeurs` (`id_editeur`, `nom_editeur`) VALUES (NULL, 'POCKET'), (NULL, 'Albin Michel'), (NULL, 'Le Livre de Poche'), (NULL, 'Gallimard'), (NULL, 'Les Editions Persée');
 INSERT INTO `roles_auteurs` (`id_role`, `role`) VALUES (NULL, 'auteur'), (NULL, 'traducteur');
