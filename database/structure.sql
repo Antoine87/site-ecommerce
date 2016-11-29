71e858e89cb9317fe52143c03b15e55bdc237818
Stéphane Emploi <moi@moi.com>
2016-11-29 13:16:42 +0100

    ajout des charset sur la création de la bd ajout de use aprés la création de la base

--- database/structure.sql
+++ database/structure.sql
@@ -2,6 +2,11 @@
 * Structure de données de l'application ecommerce
  ****************************************************/
 
+-- Suppression de la base si elle existe
 DROP DATABASE IF EXISTS ecommerce;
 
-CREATE DATABASE ecommerce;
+-- Création de la base de données
+CREATE DATABASE ecommerce
+CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+USE ecommerce;
