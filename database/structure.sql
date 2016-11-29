dde41a8cd71b252cf6b705b31f00263687d95210
sebbrec <titi@grominet.com>
2016-11-29 14:33:01 +0100

    ajout de seb

--- database/structure.sql
+++ database/structure.sql
@@ -2,80 +2,20 @@
 * Structure de données de l'application ecommerce
  ****************************************************/
 
--- Suppression de la base si elle existe
 DROP DATABASE IF EXISTS ecommerce;
 
--- Création de la base de données
-CREATE DATABASE ecommerce
-CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+CREATE DATABASE ecommerce;
 
-USE ecommerce;
-
--- -----------------------------------------------------
--- Table `ecommerce`.`clients`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`clients` (
-  `id_client` INT NOT NULL,
-  `nom` VARCHAR(45) NULL,
-  `prenom` VARCHAR(45) NULL,
-  `email` VARCHAR(100) NULL,
-  `mot_de_passe` VARCHAR(45) NULL,
-  `date_naissance` DATE NULL,
-  PRIMARY KEY (`id_client`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ecommerce`.`telephones`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`telephones` (
-  `id_telephones` INT NOT NULL,
-  `numero_telephone` VARCHAR(15) NULL,
-  `clients_id_client` INT NOT NULL,
-  PRIMARY KEY (`id_telephones`),
-  INDEX `fk_telephones_clients1_idx` (`clients_id_client` ASC),
-  CONSTRAINT `fk_telephones_clients1`
-    FOREIGN KEY (`clients_id_client`)
-    REFERENCES `ecommerce`.`clients` (`id_client`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ecommerce`.`adresses`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`adresses` (
-  `id_adresse` INT NOT NULL,
-  `adresse` VARCHAR(45) NULL,
-  `code_postal` VARCHAR(5) NULL,
-  `ville` VARCHAR(45) NULL,
-  `adresse_facturation` INT NULL,
-  `clients_id_client` INT NOT NULL,
-  PRIMARY KEY (`id_adresse`),
-  INDEX `fk_adresses_clients1_idx` (`clients_id_client` ASC),
-  CONSTRAINT `fk_adresses_clients1`
-    FOREIGN KEY (`clients_id_client`)
-    REFERENCES `ecommerce`.`clients` (`id_client`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Export de la structure de la base pour e_commerce
+CREATE DATABASE IF NOT EXISTS `e_commerce` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `e_commerce`;
 
 
 
-CREATE TABLE IF NOT EXISTS statut_de_commmande (
-    statut VARCHAR(255),
-    id_satut Tinyint unsigned auto_increment
-    PRIMARY KEY (id_statut)
-)ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `mydb`.`Coupon`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`Coupon` (
-  `idCoupon` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Date_debut` DATE NULL,
-  `Date_fin` DATE NULL,
-  `Remise` FLOAT NULL,
-  PRIMARY KEY (`idCoupon`))
-ENGINE = InnoDB;
+-- Export de données de la table e_commerce.éditeurs : ~3 rows (environ)
+DELETE FROM `éditeurs`;
+/*!40000 ALTER TABLE `éditeurs` DISABLE KEYS */;
+INSERT INTO `éditeurs` (`Nom`) VALUES
+	( 'MICHEL LAFON'),
+	( 'John Tiffany'),
+	( 'Carole Neel ');
