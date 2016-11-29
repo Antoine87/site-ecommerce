794297d7902f33b1165f501b2f9d50d597fbd03c
mehdi houfani <medmed91@gmail.com>
2016-11-29 13:08:32 +0100

    creation de la table SQL mode de paiement

--- database/structure.sql
+++ database/structure.sql
@@ -5,3 +5,16 @@
 DROP DATABASE IF EXISTS ecommerce;
 
 CREATE DATABASE ecommerce;
+
+
+/* creation de la table mode de paiement*/
+CREATE TABLE IF NOT EXISTS ecommerce.mode_de_paiement (
+  id_mode_de_paiement INT NOT NULL,
+  mode_de_paiementcol VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_mode_de_paiement),
+  UNIQUE INDEX idmode_de_paiement_UNIQUE (id_mode_de_paiement ASC))
+ENGINE = InnoDB;
+
+INSERT INTO ecommerce.mode_de_paiement (mode_de_paiementcol) VALUES ('espece');
+INSERT INTO ecommerce.mode_de_paiement (mode_de_paiementcol) VALUES ('carte bleu');
+INSERT INTO ecommerce.mode_de_paiement (mode_de_paiementcol) VALUES ('cheque');
