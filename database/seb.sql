a38e74a50ce3d63ba81f716212f8da32a189afa2
sebbrec <titi@grominet.com>
2016-11-30 09:20:47 +0100

    seb

--- database/seb.sql
+++ database/seb.sql
@@ -1,36 +1,23 @@
 -- -----------------------------------------------------
--- Table `ecommerce`.`paiement`
+-- Table `ecommerce`.`paiement`  manque 2 fk a mettre
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS`ecommerce`.`paiement` (
-	`id_commande` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`id_client` TINYTEXT NULL,
+
+
+
+CREATE TABLE `paiement` (
+	`id_commande` TINYTEXT NULL,
+	`id_client` INT(10) NULL DEFAULT NULL,
 	`montant` DECIMAL(10,0) NULL DEFAULT NULL,
 	`date de paiement` DATETIME NULL DEFAULT NULL,
-	`id_mode_paiement` TINYTEXT NULL
+	`id_mode_de_paiement` TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+	UNIQUE INDEX `id_mode_de_paiement` (`id_mode_de_paiement`),
+	CONSTRAINT `FK_paiement_mode_de_paiement` FOREIGN KEY (`id_mode_de_paiement`) REFERENCES `mode_de_paiement` (`id_mode_de_paiement`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
 
-///  les fk sont à redéfinir
-
-
-INDEX `fk_paiement_clients1` (`id_client` ASC),
-  CONSTRAINT `fk_paiment_clients1`
-    FOREIGN KEY (`id_client`)
-    REFERENCES `ecommerce`.`clients` (`id_client`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-INDEX `fk_paiement_mode_paiement1` (`id_client` ASC),
-  CONSTRAINT `fk_paiement_mode_paiement`
-    FOREIGN KEY (`id_mode_paiement`)
-    REFERENCES `ecommerce`.`clients` (`id_client`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 
