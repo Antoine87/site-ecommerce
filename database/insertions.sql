70a35951d3e02c4c72986c64e164ca99f96735fd
Stéphane Emploi <moi@moi.com>
2016-11-30 09:50:42 +0100

    modif sur les insertions et la structure sql

--- database/insertions.sql
+++ database/insertions.sql
@@ -46,3 +46,17 @@ VALUES
   ('Fillon', 'Karl', 'mail@mail.com', 'mdp123', '1950-01-01'),
   ('Mitterand', 'Sebastien', 'mail@mail.com', 'mdp123', '1968-09-01'),
   ('Pompidou', 'Antoine', 'mail@mail.com', 'mdp123', '1982-01-01');
+
+-- ------------------------------------------------------
+--  INSERT `ecommerce`.`auteurs`
+-- ------------------------------------------------------
+TRUNCATE ecommerce.auteurs;
+INSERT INTO ecommerce.auteurs
+(nom_auteur, prenom_auteur, biographie)
+VALUES
+  ('De Lafontaine', 'Jean', 'On dispose de très peu d’informations sur les années de formation de Jean de La Fontaine'),
+  ('Wilde', 'Oscar', 'Oscar Wilde est né au 21, Westland Row à Dublin'),
+  ('Herbert', 'Frank', 'Frank Herbert est né le 8 octobre 1920 à Tacoma dans l\'État de Washington, de Frank Herbert et Eileen McCarthy Herbert.'),
+  ('Maupin', 'armistead', 'Ses parents sont Diana Jane (née Barton) et Armistead Jones Maupin'),
+  ('Asimov', 'Isaac', 'Issu d’une famille juive, fils de Judah Asimov et de Anna Rachel Berman, Isaac naquit à Petrovitchi'),
+  ('Zola', 'Emile', 'Émile Édouard Charles Antoine Zola naît 10 rue Saint-Joseph à Paris, le 2 avril 1840, d\'un père italien et d\'une mère française');
