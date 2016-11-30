e9b9f8ec4790fce2a003af779410f1b6196ab148
Cedric Nicolas <moi@moi.com>
2016-11-30 09:43:00 +0100

    Merge branch 'master' of https://github.com/m2i-formation-cdi/site-ecommerce
    # Conflicts:
    #	database/structure.sql

---
+++ database/cedric.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS commentaires_livres (
+  id_commentaire INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  note INTEGER UNSIGNED NOT NULL,
+  titre_commentaire VARCHAR(255) NOT NULL,
+  date_commentaire DATE NOT NULL,
+  livre_commentaire VARCHAR(45) NOT NULL,
+  auteur_commentaire VARCHAR(45) NOT NULL,
+  PRIMARY KEY(id_commentaire),
+    CONSTRAINT livre_to_commentaire FOREIGN KEY (livre_commentaire) REFERENCES livres (id_livre),
+    CONSTRAINT client_to_commentaire FOREIGN KEY (auteur_commentaire) REFERENCES clients (id_client)
+)ENGINE = InnoDB;
+
+
+
+
