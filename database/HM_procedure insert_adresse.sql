5cf0c7181541c569f390ce88d7b2dba99693c7ce
mehdi houfani <medmed91@gmail.com>
2016-12-01 10:31:17 +0100

    MISE A JOUR procedure stocké insert livre

--- database/HM_procedure insert_adresse.sql
+++ database/HM_procedure insert_adresse.sql
@@ -3,7 +3,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `adresse_insert`(
         p_code_postal VARCHAR(5),
         p_ville VARCHAR(30),
         p_personne_id INT,
-        p_facturation TINYINT,
+        p_facturation BOOLEAN,
         OUT p_message VARCHAR(32)
 )
 BEGIN
@@ -11,13 +11,13 @@ BEGIN
 
 DECLARE v_facturation INT;
 DECLARE v_id_adresse INT;
-DECLARE v_context TINYINT;
+DECLARE v_context TINYINT DEFAULT -1;
 
 
 
 -- on test si un champ n'est pas nul
 IF p_adresse IS NOT NULL
-AND p_code_postal IS NOT NULL 
+AND p_code_postal IS NOT NULL
 AND p_ville IS NOT NULL
 AND p_personne_id IS NOT NULL
 AND p_facturation IS NOT NULL
@@ -27,93 +27,96 @@ THEN
 
 	-- verification si cette adresse exist pour ce client
 
-	SELECT ad.id_adresse 
-	FROM adresses as ad 
-	WHERE ad.id_client = p_personne_id AND ad.adresse = p_adresse AND ad.code_postal= p_code_postal 
+	SELECT ad.id_adresse
+	FROM adresses as ad
+	WHERE ad.id_client = p_personne_id AND ad.adresse = p_adresse AND ad.code_postal= p_code_postal
 	INTO v_id_adresse;
-    
 
-CASE v_id_adresse
 
--- dans le cas ou une adresse n'existe pas deja
+CASE true
+
+-- dans le cas ou cette adresse n'existe pas deja
 WHEN v_id_adresse IS NULL THEN
-    
+
 		-- si le client veut choisir cette adresse en facturation
-		IF p_facturation = 1 THEN
+		IF p_facturation IS TRUE THEN
 
 		-- on verifie si le client a une adresse de facturation
-		SELECT id_adresse FROM adresses 
-        WHERE id_client = p_personne_id AND est_adresse_facturation = '1' 
+		SELECT id_adresse FROM adresses
+        WHERE id_client = p_personne_id AND est_adresse_facturation = '1'
         INTO v_facturation;
-        
-    
-			CASE v_facturation
-				-- si le client a deja une adresse de facturation, 
-				WHEN v_facturation IS NOT NULL THEN 
+
+
+			CASE true
+				-- si le client a deja une adresse de facturation,
+				WHEN v_facturation IS NOT NULL THEN
 
 					-- on modifie son ancienne adresse de facturation a 0
-					UPDATE adresses SET est_adresse_facturation = '0' WHERE id_adresse = v_facturation;
-    
+					UPDATE adresses SET est_adresse_facturation = false WHERE id_adresse = v_facturation AND id_client = p_personne_id;
+
 					-- on insert sa nouvelle adresse de facturation dans la table
-					INSERT INTO 
+					INSERT INTO
 						adresses(adresse,code_postal, ville, est_adresse_facturation, id_client)
 					VALUES
 						(p_adresse,p_code_postal,p_ville,p_facturation,p_personne_id);
-                        
-					SET v_context := 1;
-                
+
+					SET v_context := 4;
+
 				WHEN v_facturation IS NULL THEN
-        
+
 					-- on insert sa nouvelle adresse de facturation dans la table
-					INSERT INTO 
+					INSERT INTO
 						adresses(adresse, code_postal, ville, est_adresse_facturation, id_client)
 					VALUES
 						(p_adresse,p_code_postal,p_ville,p_facturation,p_personne_id);
-                        
+
 					SET v_context := 1;
-                    
+
 				ELSE BEGIN END;
 
 
-			END CASE; 
-        
-        
+			END CASE;
+
+
 		-- si le client veut uniquement rentrer une nouvelle adresse
-		ELSEIF p_facturation = 0 THEN
-   
+		ELSEIF p_facturation IS FALSE THEN
+
 			-- on insert sa nouvelle adresse dans la table
-			INSERT INTO 
+			INSERT INTO
 				adresses(adresse, code_postal, ville, est_adresse_facturation, id_client)
 			VALUES
 				(p_adresse,p_code_postal,p_ville,p_facturation,p_personne_id);
-                
+
 			SET v_context := 2;
-			
-   
+
+
 		END IF;-- v_facturation
 
-    
+
 -- dans le cas ou l'adresse existe deja
 WHEN v_id_adresse IS NOT NULL THEN
 
 SET v_context := 3;
 
+ELSE BEGIN END;
 
-    
 END CASE;
 
 ELSE -- si un des champs est nul
 
 SET v_context := 0;
 
-END IF;-- verification d'un champ null 
+END IF;-- verification d'un champ null
 
-CASE v_context
+CASE true
 
+	WHEN v_context =-1 THEN SET p_message = "Aucune action effectué";
 	WHEN v_context = 0 THEN SET p_message = "Remplisser tous les champs";
     WHEN v_context = 1 THEN SET p_message = "Nouvelle adresse de facturation";
-	WHEN v_context = 2 THEN SET p_message = "cette adresse existe deja";
-	WHEN v_context = 3 THEN SET p_message = "Nouvelle adresse";
+	WHEN v_context = 2 THEN SET p_message = "Nouvelle adresse";
+	WHEN v_context = 3 THEN SET p_message = "cette adresse existe deja";
+    WHEN v_context = 4 THEN SET p_message = "modification de facturation";
+    ELSE BEGIN END;
     
 END CASE;
     
