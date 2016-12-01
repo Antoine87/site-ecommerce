963b68a14889a19f4f0f0ac0f6e75ff12b6a9a8c
Stéphane Emploi <moi@moi.com>
2016-12-01 08:52:14 +0100

    ajout de données pour les livres (import de fichier csv)

--- database/insertions.sql
+++ database/insertions.sql
@@ -73,3 +73,25 @@ INSERT INTO ecommerce.formats (format) VALUES ('Poche');
 INSERT INTO ecommerce.formats (format) VALUES ('Livre audio');
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+-- ------------------------------------------------------
+--  IMPORTATION DES LIVRES depuis un fichier csv
+-- ------------------------------------------------------
+LOAD DATA INFILE
+  'C:\\Users\\formation\\Documents\\PHP\\projet-ecommerce-php\\database\\data-source\\livres.csv'
+INTO TABLE livres
+FIELDS
+TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES
+TERMINATED BY '\r\n'
+IGNORE 1 LINES
+
+(titre,rubrique,id_langue,resume,
+ table_des_matieres,accroche,@dateParution,id_editeur,
+ id_collection,@dimensions,poids,nb_pages,id_format,
+ ISBN_11,ISBN_13,@prix,stock,couverture,edition)
+SET
+  prix= REPLACE(@prix,",","."),
+  date_parution=str_to_date(@dateParution,'%d/%m/%y');
