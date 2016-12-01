21ec2e45fb57a7f5b4c4661cd6cad846e0b3e18c
Stéphane Emploi <moi@moi.com>
2016-12-01 10:06:33 +0100

    Mise en place du layout de l'application

--- src/views/layout.php
+++ src/views/layout.php
@@ -5,10 +5,40 @@
     <title><?= $pageTitle ?></title>
     <link rel="stylesheet" href="/dependencies/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="/dependencies/bootstrap/dist/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body class="container fluid">
 
-<?= $pageContent ?>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Chapitre</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">catalogue</a></li>
+                <li><a href="#">mon compte</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Voir le panier</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<div class="row content">
+    <?= $pageContent ?>
+</div>
+
 <script src="/dependencies/jquery/dist/jquery.js"></script>
 <script src="/dependencies/bootstrap/dist/js/bootstrap.js"></script>
 </body>
