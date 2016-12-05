5cf7c9512fed70d8159c4666227a7b1be030a5d1
mehdi houfani <medmed91@gmail.com>
2016-12-05 13:16:12 +0100

    mise a jour securité + animation crud mode de paiement ok

--- src/controllers/mode-paiement.php
+++ src/controllers/mode-paiement.php
@@ -6,32 +6,106 @@ $idModePaiement = filter_input(INPUT_POST,'idModePaiement',FILTER_VALIDATE_INT);
 $delete = filter_input(INPUT_GET,'delete', FILTER_VALIDATE_INT);
 $update = filter_input(INPUT_POST,"update",FILTER_SANITIZE_STRING);
 $updateModePaiement = filter_input(INPUT_POST,"updateModePaiement",FILTER_SANITIZE_STRING);
-
+$message = "";
+/**
+ * procedure d'ajout d'un nouveau paiement
+ */
 if ($isSubmited){
 
-    $sql="INSERT INTO modes_de_paiement(mode_de_paiement)VALUES(?)";
+    $valide = !empty(trim($newModePaiement));
 
-    $connexion = getPDO();
+    if($valide) {
 
-    $statement = $connexion->prepare($sql);
+        try {
+            $sql = "SELECT id_mode_de_paiement FROM modes_de_paiement WHERE mode_de_paiement=?";
 
-    $statement->execute([$newModePaiement]);
+            $connexion = getPDO();
 
+            $statement = $connexion->prepare($sql);
+
+            $statement->execute([$newModePaiement]);
+
+            $valide = empty($statement->fetch(PDO::FETCH_ASSOC));
+
+        }catch (\PDOException $exception){
+
+            $message = "erreur de verification :".$exception->getMessage();
+
+        }
+
+        if ($valide) {
+
+            try {
+
+                $sql = "INSERT INTO modes_de_paiement(mode_de_paiement)VALUES(?)";
+
+                $connexion = getPDO();
+
+                $statement = $connexion->prepare($sql);
+
+                $statement->execute([strtoupper($newModePaiement)]);
+
+                $message = "ajout reussi";
+
+            } catch (\PDOException $exception) {
+
+                $message = "impossible d'inserer : ".$exception->getMessage();
+
+            }
+        }else{
+            $message = "ce mode de paiement existe deja";
+        }
+    }
 }
 
 if ($delete){
 
-    $sql="DELETE FROM modes_de_paiement WHERE id_mode_de_paiement=?";
+    try {
 
-    $connexion = getPDO();
+        $sql = "DELETE FROM modes_de_paiement WHERE id_mode_de_paiement=?";
 
-    $statement = $connexion->prepare($sql);
+        $connexion = getPDO();
 
-    $statement->execute([$delete]);
+        $statement = $connexion->prepare($sql);
+
+        $statement->execute([$delete]);
+
+        $message = "suppression effectuée";
+
+    }catch(\PDOException $exception){
+
+        $message= "impossible d'effacer :".$exception->getMessage();
+
+    }
 
 }
 
 if($update){
+
+    $valide = !empty(trim($updateModePaiement));
+
+    if ($valide) {
+
+        try {
+            $sql = "SELECT id_mode_de_paiement FROM modes_de_paiement WHERE mode_de_paiement=?";
+
+            $connexion = getPDO();
+
+            $statement = $connexion->prepare($sql);
+
+            $statement->execute([$updateModePaiement]);
+
+            $valide = empty($statement->fetch(PDO::FETCH_ASSOC));
+
+        } catch (\PDOException $exception) {
+
+            $message = "erreur de verification :" . $exception->getMessage();
+
+        }
+
+    }
+
+    if($valide){
 
     $sql = "UPDATE modes_de_paiement SET mode_de_paiement=? WHERE id_mode_de_paiement=?";
 
@@ -41,7 +115,16 @@ if($update){
 
     $statement->execute([$updateModePaiement, $idModePaiement]);
 
-}
+        $message="mise à jour ok";
+
+}else{
+
+        $message="le moyen de paiement existe déjà";
+    }
+
+};
+
+
 
     $sql = "SELECT * FROM modes_de_paiement";
 
@@ -59,4 +142,4 @@ while($row = $statement->fetch(PDO::FETCH_ASSOC)){
 
 
 
-echo renderView('view-mode-paiement',['modePaiement'=>$modePaiement]);
+echo renderView('view-mode-paiement',['modePaiement'=>$modePaiement,'message'=>$message]);
